39dc3b14431393c13559dbdfd97dc14c7c7f98b8
Daniel (dB.) Doubrovkine <dblock@dblock.org>
2025-02-01 10:59:01 -0500

    CGI escape polylines.

--- _lib/map.rb
+++ _lib/map.rb
@@ -1,3 +1,5 @@
+require 'cgi'
+
 class Strava::Models::Map < Strava::Models::Response
   def decoded_summary_polyline
     Polylines::Decoder.decode_polyline(summary_polyline) if summary_polyline
@@ -11,6 +13,6 @@ class Strava::Models::Map < Strava::Models::Response
 
     start_latlng = decoded_summary_polyline[0]
     end_latlng = decoded_summary_polyline[-1]
-    "https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&path=enc:#{summary_polyline}&key=#{google_maps_api_key}&size=800x800&markers=color:yellow|label:S|#{start_latlng[0]},#{start_latlng[1]}&markers=color:green|label:F|#{end_latlng[0]},#{end_latlng[1]}"
+    "https://maps.googleapis.com/maps/api/staticmap?maptype=roadmap&path=enc:#{CGI.escape(summary_polyline)}&key=#{google_maps_api_key}&size=800x800&markers=color:yellow|label:S|#{start_latlng[0]},#{start_latlng[1]}&markers=color:green|label:F|#{end_latlng[0]},#{end_latlng[1]}"
   end
 end
